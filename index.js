fd33b5d0b03faa50d42777b95fe1915dfe282203
Sergio <sergiazow@gmail.com>
2021-09-06 13:55:15 +0200

    add kr, es, cn, ru,nl languages

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const locales = require("./locales");
 /* auto generated file, do not touch */
 const i18n = {
   resources: {
-    /*es: {
-      translation: require('./languages/es/es.json')
-    },*/
+    es: {
+      translation: require("./languages/es/es.json"),
+    },
+
+    kr: {
+      translation: require("./languages/kr/kr.json"),
+    },
+    cn: {
+      translation: require("./languages/cn/cn.json"),
+    },
+    nl: {
+      translation: require("./languages/nkl/nkl.json"),
+    },
+    ru: {
+      translation: require("./languages/ru/ru.json"),
+    },
     en: {
       translation: require("./languages/en/en.json"),
     },
